d4aae9914839d753349e7a2043d8f2d2b6245394
Colin Jones <colin.jones@phe.gov.uk>
2015-08-13 10:21:48 +0100

    first commit for book 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b23ee760e14ed7c7633f91108a66aa842a9aedbdbb45f65cd2559f660fe2b51a6b43059ae8774ff01c84040ed439f65bd9838d0d57f54f3f9aa7ca8a5514947f'
